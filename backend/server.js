e8f4a25082b34451c97b926299821ef600c51028
JoeNarthan <77740552+JoeNarthan@users.noreply.github.com>
2025-12-13 12:21:10 +0700

    I have something

--- backend/server.js
+++ backend/server.js
@@ -51,3 +51,4 @@ app.get("/comments", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Backend running on port " + PORT));
+// I have change soething 
